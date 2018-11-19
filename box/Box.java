4ca3f2f171e68ead952d028c83df5ff03d22536a
wermergabor <gabor.wermer@greenfoxacademy.com>
2018-11-19 13:47:26 +0100

    java exercise fix

--- box/Box.java
+++ box/Box.java
@@ -1,4 +1,4 @@
-public class Cuboid {
+public class Box {
   // Create a class that represents a cuboid:
   // It should take its three dimensions as constructor parameters. For example: x: 5, y: 6, z: 15
   // It should have a method called `getSurface` that returns the cuboid's surface
